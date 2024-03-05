0af0b8b3c6f39484b7259748dce4c28dc2d17cd1
Kanani Hardik <hardik@sevensquaretech.com>
2024-03-05 17:31:07 +0530

    remove mongo connection URL

--- services/mongoose.js
+++ services/mongoose.js
@@ -4,13 +4,13 @@ const initModels = require("../models/initModels");
 const config = require("../config");
 const models = initModels(mongoose);
 
-
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}?socketTimeoutMS=360000&connectTimeoutMS=360000`, (err) => {
-  // mongoose.connect(`mongodb+srv://Nikhil:nikhil123@cluster0.r2ndn.mongodb.net/ss-voe?retryWrites=true&w=majority`, (err) => {
-
-  if (err) throw err;
-  console.log("DB Connected Successfully");
-});
+mongoose.connect(
+  `mongodb://localhost:27017/${process.env.DB_NAME}?socketTimeoutMS=360000&connectTimeoutMS=360000`,
+  (err) => {
+    if (err) throw err;
+    console.log("DB Connected Successfully");
+  }
+);
 module.exports = {
   // connectDB
 };
